22d4a78e6b26bbc77a2ec188ec6fa5eb0b33a095
Josthin Soto <josthin.soto.s@estudiantec.cr>
2023-10-25 17:52:24 -0600

    Estructura básica del servidor y calculadora funcioal

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,12 @@
 module com.example.segundoproyectodatos1 {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.scripting;
+    requires js;
 
 
     opens com.example.segundoproyectodatos1 to javafx.fxml;
+    opens servidor to java.scripting;
     exports com.example.segundoproyectodatos1;
 }
